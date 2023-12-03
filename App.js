1185cef0c3658441cae1ef589ca5ab4a13597a3a
Mursal Furqan <mursalfurqan@gmail.com>
2023-12-03 10:07:17 +0100

    Text

--- App.js
+++ App.js
@@ -30,7 +30,8 @@ export default function App() {
 
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{
+            <Stack.Navigator 
+                screenOptions={{
                     headerShown: false,
                 }}
             >
