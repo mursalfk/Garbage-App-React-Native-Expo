b13919cf3ce880185b0279e0a3fc1837ccdcdc09
Mursal Furqan <mursalfurqan@gmail.com>
2024-01-25 09:47:36 +0100

    Text

--- components/DetectGarbage.js
+++ components/DetectGarbage.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import {Button,View,Text,StyleSheet,TouchableOpacity,Image,Vibration,ActivityIndicator,
-} from "react-native";
+import {Button,View,Text,StyleSheet,TouchableOpacity,Image,Vibration,ActivityIndicator,} from "react-native";
 import { Camera, CameraType } from "expo-camera";
 import * as tf from "@tensorflow/tfjs";
 import { bundleResourceIO, decodeJpeg } from "@tensorflow/tfjs-react-native";
