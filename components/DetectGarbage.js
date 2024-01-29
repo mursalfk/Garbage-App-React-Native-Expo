3716718e162e7869aa5fcf68fcf3263ce4725431
Mursal Furqan <mursalfurqan@gmail.com>
2024-01-29 09:46:41 +0100

    Text

--- components/DetectGarbage.js
+++ components/DetectGarbage.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import {Button,View,Text,
-    StyleSheet,
+import {Button,View,Text,StyleSheet,
     TouchableOpacity,
     Image,
     Vibration,
