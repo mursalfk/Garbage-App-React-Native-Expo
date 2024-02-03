bea4e3ada33987a8e3b92d8e0aa4108eeb8f2d20
Mursal Furqan <mursalfurqan@gmail.com>
2024-02-03 09:44:27 +0100

    Text

--- App.js
+++ App.js
@@ -30,8 +30,7 @@ export default function App() {
 
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}
-            >
+            <Stack.Navigator screenOptions={{headerShown: false}}>
                 <Stack.Screen name="Landing Screen" component={LandingScreen} />
                 <Stack.Screen name="HomePage" component={HomePage} />
                 <Stack.Screen name="Leaderboard" component={Leaderboard} />
