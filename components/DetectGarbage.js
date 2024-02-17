492ab00899a6cf72d482edb46d56105b5cbd3e65
Srinjan Ghosh <srinjanghosh11@gmail.com>
2024-02-17 13:50:43 +0100

    updates

--- components/DetectGarbage.js
+++ components/DetectGarbage.js
@@ -1,8 +1,14 @@
 import React, { useState, useRef } from "react";
-import { Button, View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
-import { Camera, CameraType } from 'expo-camera';
+import {
+    Button,
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+} from "react-native";
+import { Camera, CameraType } from "expo-camera";
 import * as tf from "@tensorflow/tfjs";
-
 
 export default function DetectGarbage({ navigation }) {
     const [type, setType] = useState(CameraType.back);
@@ -15,7 +21,10 @@ export default function DetectGarbage({ navigation }) {
         return (
             <View style={styles.container}>
                 <Text>No access to camera</Text>
-                <Button title="Request Camera Permission" onPress={requestPermission} />
+                <Button
+                    title="Request Camera Permission"
+                    onPress={requestPermission}
+                />
             </View>
         );
     }
@@ -24,22 +33,27 @@ export default function DetectGarbage({ navigation }) {
         return (
             <View style={styles.container}>
                 <Text>Permission not granted</Text>
-                <Button title="Request Camera Permission" onPress={requestPermission} />
+                <Button
+                    title="Request Camera Permission"
+                    onPress={requestPermission}
+                />
             </View>
         );
     }
 
     const showLeaderboard = () => {
         console.log("Leaderboard Button Clicked");
-        navigation.navigate('Leaderboard');
+        navigation.navigate("Leaderboard");
     };
 
     const homePageButton = () => {
-        navigation.navigate('HomePage');
+        navigation.navigate("HomePage");
     };
 
     function toggleCameraType() {
-        setType(current => (current === CameraType.back ? CameraType.front : CameraType.Back))
+        setType((current) =>
+            current === CameraType.back ? CameraType.front : CameraType.Back
+        );
     }
 
     const takePicture = async () => {
@@ -48,14 +62,14 @@ export default function DetectGarbage({ navigation }) {
             const data = await cameraRef.current.takePictureAsync(options);
             setCapturedImage(data.uri);
             setIsLive(false);
-            predictImage()
+            predictImage();
         }
     };
 
     const predictImage = async () => {
-        const model = await tf.loadLayersModel("../model/model.json")
-        console.log(model)
-    }
+        const model = await tf.loadLayersModel("model/model.json");
+        console.log("model loaded");
+    };
 
     const retakePicture = () => {
         setCapturedImage(null);
@@ -66,29 +80,47 @@ export default function DetectGarbage({ navigation }) {
         <View style={styles.container}>
             {isLive ? (
                 <View style={styles.cameraContainer}>
-                    <Camera style={styles.camera} type={type} ref={cameraRef}>
-                    </Camera>
+                    <Camera
+                        style={styles.camera}
+                        type={type}
+                        ref={cameraRef}
+                    ></Camera>
                     <View style={styles.buttonsContainer}>
-                        <TouchableOpacity style={styles.clickPhotoButton} onPress={takePicture}>
+                        <TouchableOpacity
+                            style={styles.clickPhotoButton}
+                            onPress={takePicture}
+                        >
                             <Text style={styles.buttonText}>Click Picture</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
             ) : (
                 <View style={styles.imageContainer}>
-                    <Image source={{ uri: capturedImage }} style={styles.capturedImage} />
+                    <Image
+                        source={{ uri: capturedImage }}
+                        style={styles.capturedImage}
+                    />
                     <View style={styles.buttonsContainer}>
-                        <TouchableOpacity style={styles.retakeButton} onPress={retakePicture}>
+                        <TouchableOpacity
+                            style={styles.retakeButton}
+                            onPress={retakePicture}
+                        >
                             <Text style={styles.buttonText}>Retake</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
             )}
             <View style={styles.bottomButtonsContainer}>
-                <TouchableOpacity style={styles.leaderboardButton} onPress={showLeaderboard}>
+                <TouchableOpacity
+                    style={styles.leaderboardButton}
+                    onPress={showLeaderboard}
+                >
                     <Text style={styles.buttonText}>Leaderboard</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.homeButton} onPress={homePageButton}>
+                <TouchableOpacity
+                    style={styles.homeButton}
+                    onPress={homePageButton}
+                >
                     <Text style={styles.buttonText}>Back to Homepage</Text>
                 </TouchableOpacity>
             </View>
@@ -99,71 +131,71 @@ export default function DetectGarbage({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
     },
     cameraContainer: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
     },
     camera: {
-        display: 'flex',
-        height: '60%',
-        width: '100%',
-        justifyContent: 'center',
+        display: "flex",
+        height: "60%",
+        width: "100%",
+        justifyContent: "center",
     },
     buttonsContainer: {
-        position: 'absolute',
+        position: "absolute",
         bottom: 0,
         left: 0,
         right: 0,
-        flexDirection: 'row',
-        justifyContent: 'center',
+        flexDirection: "row",
+        justifyContent: "center",
         paddingBottom: 20,
     },
     clickPhotoButton: {
-        backgroundColor: 'green',
+        backgroundColor: "green",
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 25,
     },
     imageContainer: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
     },
     capturedImage: {
-        display: 'flex',
-        height: '60%',
-        width: '100%',
-        justifyContent: 'center',
+        display: "flex",
+        height: "60%",
+        width: "100%",
+        justifyContent: "center",
     },
     retakeButton: {
-        backgroundColor: 'green',
+        backgroundColor: "green",
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 25,
     },
     bottomButtonsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        justifyContent: "space-between",
         paddingHorizontal: 20,
         paddingBottom: 20,
     },
     leaderboardButton: {
-        backgroundColor: 'green',
+        backgroundColor: "green",
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 5,
     },
     homeButton: {
-        backgroundColor: 'green',
+        backgroundColor: "green",
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 5,
     },
     buttonText: {
-        color: 'white',
-        fontWeight: 'bold',
+        color: "white",
+        fontWeight: "bold",
     },
 });
